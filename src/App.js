9ea7ccc61d8d06af2cc3088767f86092c0f4f821
tusharahujaa <83968279+tusharahujaa@users.noreply.github.com>
2022-09-07 04:41:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import SideBar from './Components/SideBar/SideBar';
+
+function App() {
+  return (
+    <div className="App">
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+      
+      <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-primary">Left</button>
+        <button type="button" class="btn btn-primary">Middle</button>
+        <button type="button" class="btn btn-primary">Right</button>
+      </div>
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/SideBar/SideBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+const SideBar =() => {
+    return(
+        <div clasName="SideBar_container">
+            <div className="SideBar_HomeIcon">
+                <TwitterIcon />
+            </div>
+  
+        </div>
+    )
+}
+
+export default SideBar
